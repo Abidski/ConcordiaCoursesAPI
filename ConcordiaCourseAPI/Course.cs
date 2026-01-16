0f63e8b3b4f2472f7a0e4f953a6b0ef0a81fd6e9
Abid <abijanahmed@gmail.com>
2026-01-16 18:05:57 -0500

    Add put, post, get and delete handlers.

--- ConcordiaCourseAPI/Course.cs
+++ ConcordiaCourseAPI/Course.cs
@@ -2,6 +2,7 @@ namespace ConcordiaCourseAPI;
 
 public class Course
 {
+   public int Id { get; set; }
    public string Department { get; set; } 
    public int Code { get; set; } 
    public string Location { get; set; }
--- ConcordiaCourseAPI/Program.cs
+++ ConcordiaCourseAPI/Program.cs
@@ -8,6 +8,65 @@ var app = builder.Build();
 
 var courses = app.MapGroup("/courses");
 
-courses.MapGet("/", async (CourseDb db)  => await db.Courses.ToListAsync());
+courses.MapGet("/", async (CourseDb db)  =>
+{
+    var result = db.Courses.ToListAsync();
+    return result;
+});
+
+courses.MapGet("/{id}", async (CourseDb db, int id)  =>
+{
+   var result = await db.Courses.FindAsync(id);
+   if (result == null)
+   {
+       return Results.NotFound();
+   }
+
+   return Results.Ok(result);
+});
+
+courses.MapPost("/", async (Course course, CourseDb db) =>
+{
+
+    db.Courses.Add(course);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/courses/{course.Id}", course);
+});
+
+courses.MapPut("/{id}", async (Course course, CourseDb db) =>
+{
+    var result = await db.Courses.FindAsync(course.Id);
+
+    if (result == null)
+    {
+        return Results.NotFound();
+    }
+
+    result.Department = course.Department;
+    result.Code = course.Code;
+    result.Location = course.Location;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(course);
+
+});
+
+courses.MapDelete("/{id}" , async (CourseDb db) => {
+    
+    var result = await db.Courses.FindAsync(course.Id);
+
+    if (result == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Remove(result);
+    await db.SaveChangesAsync();
+    
+    return Results.Ok("Course deleted");
+
+});
 
 app.Run();
